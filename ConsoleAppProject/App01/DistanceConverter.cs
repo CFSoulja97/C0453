97fbb2a5804dcca7e11d0397c931560726a28085
CFSoulja97 <100122506@student.burnley.ac.uk>
2024-07-10 17:31:51 +0100

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppProject.App01
     /// The purpose of this app is convert distance.
     /// </summary>
     /// <author>
-    /// Liam Lopez
+    /// Liam Lopez 0.1
     /// </author>
     public class DistanceConverter
     {
@@ -19,29 +19,35 @@ namespace ConsoleAppProject.App01
         /// </summary>
         public void Run()
         {
-            InputMiles();
-            CalculateFeet();
-            OutputFeet();
+
         }
+        
         /// <summary>
         /// Prompt the user to enter the distance in miles
         /// Input the miles as a double number
         /// </summary>
         private void InputMiles()
         {
-            Console.Write("Please enter the number of miles  >   ");
-            string value = Console.ReadLine();
-            miles = Convert.ToDouble(value);
+
         }
-        
+
+        /// <summary>
+        /// 
+        /// </summary>
         private void CalculateFeet()
         {
 
         }
-        
+
+        /// <summary>
+        /// 
+        /// </summary>
+
         private void OutputFeet()
         {
 
         }
+
+     
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,6 +1,4 @@
 ﻿using ConsoleAppProject.App01;
-using ConsoleAppProject.App03;
-using ConsoleAppProject.Helpers;
 using System;
 
 namespace ConsoleAppProject
@@ -11,7 +9,7 @@ namespace ConsoleAppProject
     /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    /// Liam Lopez 02/02/2024
     /// </summary>
     public static class Program
     {
@@ -19,14 +17,12 @@ namespace ConsoleAppProject
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine("BNU CO453 Applications Programming 2022-2023!");
             Console.WriteLine();
-            Console.WriteLine(" =================================================");
-            Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! ");
-            Console.WriteLine(" =================================================");
-            Console.WriteLine();
+            Console.Beep(); 
 
             DistanceConverter converter = new DistanceConverter();
-            //converter.run();
+            converter.Run();
         }
     }
 }
